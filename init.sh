3fdbab16e9ddc0a1154ec96f46b89f15a4c0ce90
Szabó Gyula <gyufi@szabocsalad.com>
2017-01-04 23:15:18 +0100

    shib restart for correct metarefresh

--- init.sh
+++ init.sh
@@ -6,14 +6,17 @@ if [[ -n "$DOCUMENT_ROOT" ]]; then
 fi
 
 echo start apache2
-export
-apachectl -D BACKGROUND
+service apache2 start
+
 echo start shibd
 service shibd start
+
 echo refresh metadata
 curl -v -k 'https://project.local/Shibboleth.sso/Metadata' -o /var/simplesamlphp/simplesamlphp-1.14.11/metadata/sp-metadata.xml
-#curl -v -k 'https://project.local/simplesaml/module.php/cron/cron.php?key=secret&tag=daily'
-ls -la /var/simplesamlphp/simplesamlphp-1.14.11/metadata
+
+echo restart shibd
+service shibd start
+
 echo start tailon
 cat /etc/tailon.yml
 tailon -c /etc/tailon.yml
