8d2de9e5bd4f8e9d2f1804ca4509bb5a02cebd13
Szabó Gyula <gyufi@szabocsalad.com>
2017-01-19 17:31:59 +0100

    hostname from environment variable

--- README.md
+++ README.md
@@ -22,6 +22,12 @@ Run
 __Set resolving__
  Edit your `/etc/hosts` file in your machine. All hostnames `project.local` must show to local Docker IP address, which is usually `192.168.99.100 project.local` or on Ubuntu 16.04 `127.0.0.1 project.local`
 
+You can override the default project.local hostname by environment variable: `HOSTNAME`
+
+example:
+
+`docker run -d -p 80:80 -p 443:443 -p 8080:8080 -e HOSTNAME=example.com -v /path/to/webapp_project:/var/www/project szabogyula/saml-webapp-runner:ubuntu16.04`
+
 __Run the docker container__
 Run for development (adjust the example hostnames before running!): `docker run -d -p 80:80 -p 443:443 -p 8080:8080 --add-host project.local:127.0.0.1
  -v /path/to/webapp_project:/var/www/project szabogyula/saml-webapp-runner:ubuntu16.04
@@ -32,6 +38,7 @@ You can set the relative document root instead of `web` by set `DOCUMENT_ROOT` e
 
 __Access the webapp__
 * 80 and 443 port is the application, and there is a tailon at 8080 port
+
  
 Development
 -----------
--- init.sh
+++ init.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
 
+echo Set hostname from environment variable
+
+if [[ -n "$HOSTNAME" ]]; then
+echo -- hostname
+	echo 127.0.0.1 "$HOSTNAME" >> /etc/hosts
+
+	echo -- apache
+	sed -i -e "s/project.local/$HOSTNAME/g" /etc/apache2/sites-available/000-default.conf
+
+	echo -- shibboleth cert
+	openssl req -new -nodes -x509 -subj "/C=HU/ST=Budapest/L=Budapest/O=IT/CN=$HOSTNAME" -days 3650 -keyout /etc/shibboleth/project.shibboleth.key -out /etc/shibboleth/project.shibboleth.crt
+
+	echo -- shibboleth2.xml
+	sed -i -e "s/project.local/$HOSTNAME/g" /etc/shibboleth/shibboleth2.xml
+
+	echo -- ssp cert
+	openssl req -new -nodes -x509 -subj "/C=HU/ST=Budapest/L=Budapest/O=IT/CN=$HOSTNAME" -days 3650 -keyout /var/simplesamlphp/simplesamlphp-1.14.11/cert/server.pem -out /var/simplesamlphp/simplesamlphp-1.14.11/cert/server.crt
+
+	echo -- ssp authsources
+	sed -i -e "s/project.local/$HOSTNAME/g" /var/simplesamlphp/simplesamlphp-1.14.11/config/authsources.php
+
+	echo -- ssp idp hosted
+	sed -i -e "s/project.local/$HOSTNAME/g" /var/simplesamlphp/simplesamlphp-1.14.11/metadata/saml20-idp-hosted.php
+
+fi
+
+
 echo Set relative document root
 if [[ -n "$DOCUMENT_ROOT" ]]; then
 	sed -i -e "s#/var/www/project/web#/var/www/project/$DOCUMENT_ROOT#g" /etc/apache2/sites-available/000-default.conf
 fi
+
+chown -R www-data /tmp/cache /tmp/logs
 
 echo start apache2
 apachectl start
@@ -12,8 +41,11 @@ echo start shibd
 service shibd start
 
 echo refresh metadata
-curl -v -k 'https://project.local/Shibboleth.sso/Metadata' -o /var/simplesamlphp/simplesamlphp-1.14.11/metadata/sp-metadata.xml
-
+if [[ -n "$HOSTNAME" ]]; then
+	curl -v -k https://"$HOSTNAME"/Shibboleth.sso/Metadata -o /var/simplesamlphp/simplesamlphp-1.14.11/metadata/sp-metadata.xml
+else
+	curl -v -k 'https://project.local/Shibboleth.sso/Metadata' -o /var/simplesamlphp/simplesamlphp-1.14.11/metadata/sp-metadata.xml
+fi
 echo restart shibd
 service shibd start
 
